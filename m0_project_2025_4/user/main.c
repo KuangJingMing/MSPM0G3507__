b76f44c3204683fe00eaafb57e743ea26239497e
胡鸿光 <3521603185@qq.com>
2025-04-15 17:44:56 +0800

    实现编码器功能+oled的spi引脚配置

--- m0_project_2025_4/user/main.c
+++ m0_project_2025_4/user/main.c
@@ -9,7 +9,7 @@ int main(void)
     SYSCFG_DL_init();
 		Gray_Detection_Init();
 		OLED_Init();
-		OLED_Test();
+		//OLED_Test();
 	  Motor_Init();//电机初始化
 		Encoder_init();//编码器初始化
 	  Motor_Control(1200, 1200);
@@ -20,10 +20,10 @@ int main(void)
 			//Gray_ReadData();
 			//delay_ms(1000);
 			delay_ms(20);
-//			OLED_Showdecimal(0,0,right,4,2,16,1);
-//	    OLED_Showdecimal(1,3,left,4,2,16,1);
-//	    OLED_ShowNum(2,6,left_motor_period_cnt,4,16,1);
-//	    OLED_ShowNum(60,6,right_motor_period_cnt,4,16,1);
+			OLED_Showdecimal(0,0,right,4,2,16,1);
+	    OLED_Showdecimal(1,3,left,4,2,16,1);
+	    OLED_ShowNum(2,6,left_motor_period_cnt,4,16,1);
+	    OLED_ShowNum(60,6,right_motor_period_cnt,4,16,1);
     }
     return 0;
 }
